d3a7439f97bc7b190f9b0db982794da3cfe5315a
Ninocabanero <cabaneronino50@gmail.com>
2024-11-15 20:17:24 +0800

    update

--- constants/images.js
+++ constants/images.js
@@ -1,9 +1,11 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
+import profile from "../assets/images/profile.jpg";
 import cards from "../assets/images/cards.png";
 import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import studlogo from "../assets/images/studlogo.png";
+import logo from "../assets/images/logo.png";
+import meme from "../assets/images/meme.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+
+
+export default { profile, cards, path, empty, studlogo, logo, meme};
